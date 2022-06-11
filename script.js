cae58c0dccff5d8d8562d59080e1809ac9f68c64
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2022-06-11 21:46:09 +0100

    add posts.html page

---
+++ script.js
@@ -0,0 +1,10 @@
+let viewportHeight = window.innerHeight;
+let viewportWidth = window.innerWidth;
+
+let side_right = document.getElementById('side-right');
+
+// make page proportionate size throughout resize like cssgrids.com
+function fixSideSize() {
+    let scale = viewportHeight / window.innerHeight;
+
+}
--- todo.md
+++ todo.md
@@ -1,1 +1,3 @@
 ⬜ write md to html function
+⬜ create md blog entry page
+⬜ write custom citation markdown and convert to italics in html
