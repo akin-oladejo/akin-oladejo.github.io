37e0b3bcd8c738b2e6b59c21a056b99c70d7d3f1
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2022-07-15 00:49:12 +0100

    change default image url

--- app/schemas.py
+++ app/schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, HttpUrl
 
 class Image(BaseModel):
     image_caption: str
-    image_url: UploadFile | HttpUrl = 'https://images.unsplash.com/photo-1512682479844-0fa51f42b4a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJsYWNrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+    image_url: UploadFile | HttpUrl = 'app\pic3.jpg'
     image_name: str
 
 class User(BaseModel):
