00d4cd3061a48e812fb60fe4b66dd7c2a5896402
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2022-07-15 21:26:00 +0100

    create comment schema

--- app/schemas.py
+++ app/schemas.py
@@ -10,6 +10,13 @@ class Image(BaseModel):
 class User(BaseModel):
     pass
 
+class CommentBase(BaseModel):
+    author: User
+    comment: str
+
+class CommentIn(CommentBase):
+    pub_datetime: datetime = Field(default_factory=datetime.now)
+
 class BlogBase(BaseModel):
     image: Image
     title: str
