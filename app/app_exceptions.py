fe9a401679379fb9082b32040132ec3f6b926a33
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2022-07-20 20:31:37 +0100

    create upload error exception handler

--- app/app_exceptions.py
+++ app/app_exceptions.py
@@ -1,7 +1,7 @@
 from fastapi import status, Request
 from fastapi.responses import JSONResponse
 
-class ImageFormatException(Exception):
-    def __init__(self, file_name:str) -> None:
+class UploadFormatException(Exception):
+    def __init__(self, file_name:str, error_message:str) -> None:
         self.file_name = file_name
-    
+        self.error_message = error_message
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .routers import blog
-from .app_exceptions import ImageFormatException
+from .app_exceptions import UploadFormatException
 from fastapi.responses import JSONResponse
 from fastapi import Request, status
 from . import models
@@ -17,9 +17,9 @@ app.include_router(blog.router)
 # load up and refresh the database
 models.Base.metadata.create_all(bind=engine)
 
-@app.exception_handler(ImageFormatException)
-def image_format_handler(request:Request, exc: ImageFormatException):
+@app.exception_handler(UploadFormatException)
+def upload_format_handler(request:Request, exc: UploadFormatException):
     return JSONResponse(
         status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        content={'detail':f"'{exc.file_name}' is not a valid (image) format. Current supported image types are PNG and JPG."}
+        content={'detail':f"There is a problem with '{exc.file_name}'. {str.capitalize(exc.error_message)}"}
     )
