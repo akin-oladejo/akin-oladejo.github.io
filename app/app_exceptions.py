9b421ddffc9bd561ebe0849fbcfa3673feda70ae
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2022-07-27 18:32:03 +0100

    refactor: delete app_exceptions.py

--- app/app_exceptions.py
+++
@@ -1,7 +0,0 @@
-from fastapi import status, Request
-from fastapi.responses import JSONResponse
-
-class UploadFormatException(Exception):
-    def __init__(self, file_name:str, error_message:str) -> None:
-        self.file_name = file_name
-        self.error_message = error_message
